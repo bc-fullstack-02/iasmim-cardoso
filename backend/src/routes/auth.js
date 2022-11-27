315f1358827310137d325aebb3c5e1fe05040837
iasarchi <iasmimcardoso11@gmail.com>
2022-11-27 19:06:44 -0300

    autenticacao do login

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -25,6 +25,20 @@ router.post("/register", async (req,res)=>{
             console.log(err);
         }
     });
+    //LOGIN
+    router.post("/login", async(req,res)=>{
+        try{    
+            const user = await User.findOne({email:req.body.email});
+            !user && res.status(404).send("User not found")
+
+            const validPassword = await bcrypt.compare(req.body.password, user.password)
+            !validPassword && res.status(400).json("Wrong Password")
+
+            res.status(200).json(user)
+        }catch(err){
+            console.log(err);
+        }
+    })
 
 module.exports = router;
 
