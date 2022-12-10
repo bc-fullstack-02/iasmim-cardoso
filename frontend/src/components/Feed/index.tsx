254cb0374b943ee9d6a1c6484a9f57c179f65dc0
iasarchi <iasmimcardoso11@gmail.com>
2022-12-10 12:39:05 -0300

    implementacao de likes no feed

--- frontend/src/components/Feed/index.tsx
+++ frontend/src/components/Feed/index.tsx
@@ -17,20 +17,43 @@ interface Post {
 
 function Feed() {
   const token = localStorage.getItem("token");
-  const [posts, setPosts] = useState <Post | []>([]);
+  const user = localStorage.getItem("user");
+  const profile = localStorage.getItem("profile");
+  const [posts, setPosts] = useState<Post[]>([]);
+  const authHearder = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
   useEffect(() => {
     async function getPosts() {
-      const response = await api.get("/feed", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
+      const response = await api.get("/feed", authHearder);
       setPosts(response.data);
     }
     getPosts();
   }, []);
+
+  async function handleLike(postId: String) {
+    try {
+      await api.post(`/posts/${postId}/like`, null, authHearder);
+      const newPost = posts
+        .filter((post) => post._id === postId)
+        .map((post) => {
+          post.likes.push(profile);
+          return post;
+        });
+
+      setPosts((posts) => {
+        const post = newPost[0];
+        const index = posts.indexOf(post);
+        posts[index] = post;
+        return [...posts];
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   console.log(posts);
 
@@ -42,34 +65,42 @@ function Feed() {
         </Text>
         <div className="flex flex-row items-center ml-5 my-4">
           <UserCircle size={48} weight="light" className="text-slate-50" />
-          <Text className="font-extrabold ml-2">Fulano da silva</Text>
+          <Text className="font-extrabold ml-2">{user}</Text>
         </div>
       </Heading>
       <section>
-        {posts.map((post) => (
-          <div className="border-b border-slate-400" key={post._id}>
-            <div className="flex flex-row items-center ml-5 my-4">
-              <UserCircle size={48} weight="light" className="text-slate-50" />
-              <Text className="font-extrabold ml-2">{post.profile.name}</Text>
-            </div>
-            <div className="ml-16 flex flex-col gap-2">
-              <Heading size="sm">{post.title}</Heading>
-              <Text asChild >
-                <p>{post.description}</p>
-              </Text>
-            </div>
-            <div className="flex items-center ml-16 my-4 space-x-2">
-              <Chat size={24} className="text-slate-50" />
-              <Text size="sm">{post.comments.length}</Text>
-
-              <div className="hover:bg-sky-400 rounded-full p-1">
-                <Heart size={24} className="text-slate-50" />
+        {posts &&
+          posts.map((post: Post) => (
+            <div className="border-b border-slate-400" key={post._id}>
+              <div className="flex flex-row items-center ml-5 my-4">
+                <UserCircle
+                  size={48}
+                  weight="light"
+                  className="text-slate-50"
+                />
+                <Text className="font-extrabold ml-2">{post.profile.name}</Text>
               </div>
+              <div className="ml-16 flex flex-col gap-2">
+                <Heading size="sm">{post.title}</Heading>
+                <Text asChild>
+                  <p>{post.description}</p>
+                </Text>
+              </div>
+              <div className="flex items-center ml-16 my-4 space-x-2">
+                <Chat size={24} className="text-slate-50" />
+                <Text size="sm">{post.comments.length}</Text>
 
-              <Text size="sm">{post.likes.length}</Text>
+                <div
+                  className="hover:bg-sky-400 rounded-full p-1"
+                  onClick={() => handleLike(post._id)}
+                >
+                  <Heart size={24} className="text-slate-50" />
+                </div>
+
+                <Text size="sm">{post.likes.length}</Text>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </section>
     </div>
   );
